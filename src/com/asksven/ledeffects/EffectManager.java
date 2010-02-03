13b381beb2248b9573d1e57a9e393da5893ef3f8
sven <sven@1213173a-3874-46ad-ac8a-7a705b046ef2>
2010-02-03 22:42:48 +0000

    refactored EffectManager and added notification when applying effect

--- src/com/asksven/ledeffects/EffectManager.java
+++ src/com/asksven/ledeffects/EffectManager.java
@@ -2,6 +2,12 @@ package com.asksven.ledeffects;
 
 import java.io.DataOutputStream;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 public class EffectManager
 {
 	public static final String EFFECT_NONE 		= "0";
@@ -12,26 +18,55 @@ public class EffectManager
 	public static final String EFFECT_ROTATE 	= "5";
 	public static final String EFFECT_VERTICAL	= "6";
 	
+	private static String m_strCurrentState		= EFFECT_NONE;
+	
 	private static final String FILE_EFFECTS 	= "/dbgfs/micropklt_dbg/effects";
 	
 	/** Apply the effect by echoing to FILE_EFFECTS */
-	public static void doEffect(String strEffect)
+	public static boolean doEffect(Context ctx, String strEffect)
 	{
-		try
+		boolean bChanged = false;
+		
+		if (!strEffect.equals(m_strCurrentState))
 		{
-			// dirty hack: http://code.google.com/p/market-enabler/wiki/ShellCommands
-			Process process = Runtime.getRuntime().exec("su");
-			DataOutputStream os = new DataOutputStream(process.getOutputStream());
-			String strCommand = "echo " + strEffect + " > " + FILE_EFFECTS;
-			os.writeBytes(strCommand + "\n");
-			os.flush();
-			os.writeBytes("exit\n");
-			os.flush();
-			process.waitFor();
+			bChanged = true;
+			m_strCurrentState = strEffect;
+			try
+			{
+				// dirty hack: http://code.google.com/p/market-enabler/wiki/ShellCommands
+				Process process = Runtime.getRuntime().exec("su");
+				DataOutputStream os = new DataOutputStream(process.getOutputStream());
+				String strCommand = "echo " + strEffect + " > " + FILE_EFFECTS;
+				os.writeBytes(strCommand + "\n");
+				os.flush();
+				os.writeBytes("exit\n");
+				os.flush();
+				process.waitFor();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				if (!strEffect.equals(EFFECT_NONE))
+				{
+					NotificationManager mNM = (NotificationManager)ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+					String strNote = "Applying effect " + strEffect;
+			    	Notification notification = new Notification(R.drawable.icon, strNote, System.currentTimeMillis());
+			    	PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
+			                new Intent(ctx, MainAct.class), 0);
+			    	notification.setLatestEventInfo(ctx, ctx.getText(R.string.local_service_label), strNote, contentIntent);
+			    	mNM.notify(R.string.app_name, notification);
+				}
+	
+			}
 		}
-		catch (Exception e)
+		else
 		{
-			e.printStackTrace();
+			bChanged = false;
 		}
+		
+		return bChanged;
 	}
 }
