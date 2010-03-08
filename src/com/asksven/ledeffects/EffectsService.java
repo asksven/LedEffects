bbdcc465b70b1f6627c6758d663731a700f56c2f
sven <sven@1213173a-3874-46ad-ac8a-7a705b046ef2>
2010-03-08 20:58:29 +0000

    Added own XMPP Client

--- src/com/asksven/ledeffects/EffectsService.java
+++ src/com/asksven/ledeffects/EffectsService.java
@@ -3,19 +3,30 @@ package com.asksven.ledeffects;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.app.Notification;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.ConnectionListener;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.MessageTypeFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.util.StringUtils;
+
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
+import android.os.Handler;
 
 import com.asksven.ledeffects.data.Preferences;
 import com.asksven.ledeffects.manager.EffectsFassade;
+import com.asksven.ledeffects.xmmp.XmppClient;
 
 /**
  * The LedEffects Service keeps running even if the main Activity is not displayed/never called
@@ -32,9 +43,14 @@ public class EffectsService extends Service
 	/** the timer update freq. in ms */
 	private long m_lUpdateInterval = 30*1000;
 	
+	/** should we connect xmpp? */
+	boolean m_bXmppConnect = false;
+	
 	private BatteryBroadcastHandler m_oBatHandler = null;
 	private boolean m_bRegistered = false;
 	
+	/** Optional XMPP client managed by service */
+	private XmppClient m_myXmppClient;
 	
     /**
      * Class for clients to access.  Because we know this service always
@@ -65,7 +81,8 @@ public class EffectsService extends Service
         	m_oBatHandler = new BatteryBroadcastHandler();
             registerReceiver(m_oBatHandler, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
             m_bRegistered = true;
-        }        
+        }
+        m_myXmppClient = new XmppClient(this);
     }
 
     /** 
@@ -75,6 +92,15 @@ public class EffectsService extends Service
     {
         // read the Preferences upon start
     	loadPrefs();
+    	
+    	// connect to xmpp
+    	if (m_bXmppConnect)
+    	{
+    		m_myXmppClient.connect();
+    	}
+    	
+    	// run the timer
+// timer's not needed    	startTimer();
     	
         Log.i(getClass().getSimpleName(), "Received start id " + startId + ": " + intent);
         // We want this service to continue running until it is explicitly
@@ -94,6 +120,8 @@ public class EffectsService extends Service
         unregisterReceiver(m_oBatHandler);
 
         stopTimer();
+        
+        m_myXmppClient.disconnect();
     }
 
     @Override
@@ -113,7 +141,7 @@ public class EffectsService extends Service
     {
 		// Apply the effect for current state
     	EffectsFassade.getInstance().doEffect(this);
-		
+    	Preferences myPrefs = new Preferences(this.getSharedPreferences(Preferences.PREFS_NAME, 0));
     }
     
     /**
@@ -148,9 +176,9 @@ public class EffectsService extends Service
     {
         Preferences myPrefs = new Preferences(this.getSharedPreferences(Preferences.PREFS_NAME, 0));
         m_lUpdateInterval 	= myPrefs.getPollInterval() * 1000; // as timer is in ms
+        m_bXmppConnect = myPrefs.getXmppConnect();
         myPrefs.applySleep();
 
     }
-    
 }
 
