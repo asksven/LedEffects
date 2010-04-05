942cddb8276c0aa6479ee61bb0df40f2cdac7ff2
sven <sven@1213173a-3874-46ad-ac8a-7a705b046ef2>
2010-04-05 09:46:07 +0000

    added tar.gz for apk as well

--- tar_sources.sh
+++ tar_sources.sh
@@ -3,6 +3,7 @@
 if [ -d ./LedEffects ]; then 
   VERSION=`cat LedEffects/AndroidManifest.xml | grep "versionName" | sed 's/"/ /g' | awk {'print $2'}`
   tar -czf LedEffects_src_${VERSION}.tar.gz ./LedEffects/ --exclude=./LedEffects/bin/*
+  tar -czf LedEffects_${VERSION}.tar.gz ./LedEffects/bin/LedEffects.apk
 else
   echo "you must be in your workspace dir to tar sources"
 fi
