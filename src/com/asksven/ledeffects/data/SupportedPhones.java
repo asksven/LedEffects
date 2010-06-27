675882ff16335e223defb37ac8d19f6b6cb2c58b
sven <sven@1213173a-3874-46ad-ac8a-7a705b046ef2>
2010-06-27 10:53:07 +0000

    added desire

---
+++ src/com/asksven/ledeffects/data/SupportedPhones.java
@@ -0,0 +1,13 @@
+package com.asksven.ledeffects.data;
+
+/** constant pattern
+ * enumeration of supported phones 
+ * @see resource "phones" in array.xml */
+public class SupportedPhones
+{
+	public static final int RAPH_DIAM			= 0;
+	public static final int TOPA				= 1;
+	public static final int GENERIC				= 2;
+	public static final int NEXUS_ONE_ROOTED	= 3;
+	public static final int DESIRE_UNROOTED		= 4;
+}
